4decad15c2cc7e0b7749f688e92858ba9f5ebd6d
Luca <luca@lambia.it>
2017-09-30 11:03:14 +0200

    Router and JSON

--- comphare.go
+++ comphare.go
@@ -11,10 +11,18 @@ package main
 import "fmt"
 import "io/ioutil"
 import "log"
+import "net/http"
+import "github.com/gorilla/mux"
+import "encoding/json"
 
 func main() {
-	//fmt.Printf("hello, world\n")
+    router := mux.NewRouter().StrictSlash(true)
+    router.HandleFunc("/", IndexHandler)
+    router.HandleFunc("/file/{fileName}", FileHandler)
+	fmt.Printf("Comphare serving on\r\nhttp://localhost:80\r\n")
+	log.Fatal(http.ListenAndServe(":80", router))
 
+	/*
 	x := []byte("test123")
 	err := save("file.ext",x)
 	if err != nil {
@@ -25,8 +33,32 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	} else {
-		fmt.Printf(string(y[:]))		
+		fmt.Printf(string(y[:]))
 	}
+	*/
+}
+
+type DumpFile struct {
+	Filename      string    `json:"filename"`
+    Content       []byte	`json:"content"`
+}
+type DumpFiles []DumpFile
+
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintln(w, "Welcome!")
+}
+
+func FileHandler(w http.ResponseWriter, r *http.Request) {
+    vars := mux.Vars(r)
+    fileName := vars["fileName"]
+	fmt.Fprintln(w, "File name:", fileName)
+	
+	todos := DumpFiles{
+        DumpFile{Filename: "test.mfd", Content: []byte{'t', 'e', 's', 't'} },
+        DumpFile{Filename: "file.ext", Content: []byte{'f', 'i', 'l', 'e'} },
+    }
+
+    json.NewEncoder(w).Encode(todos)
 }
 
 func save(filename string, content []byte) error {
